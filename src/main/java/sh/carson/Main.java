08f31ffa525135dd0e5e305f5b861f4d2522a771
Carson Graham <carson42g@gmail.com>
2024-01-22 01:31:16 -0500

    Hello World -> Hi World

--- src/main/java/sh/carson/Main.java
+++ src/main/java/sh/carson/Main.java
@@ -2,6 +2,6 @@ package sh.carson;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hi World!");
     }
 }
